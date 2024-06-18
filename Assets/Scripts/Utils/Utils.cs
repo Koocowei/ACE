a896a46d0a4146af1304f694b6c007c3489c8f30
jimmy <jimmy@kooco.com.tw>
2024-06-18 17:32:26 +0800

    新增Utils url載入圖片

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -1,13 +1,50 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 using ZXing;
 using ZXing.QrCode;
 
 public static class Utils
 {
+    /// <summary>
+    /// 載入url圖片
+    /// </summary>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public static async Task<Sprite> ImageUrlToSprite(string url)
+    {
+        if (string.IsNullOrEmpty(url))
+        {
+            return null;
+        }
+
+        Sprite sprite = null;
+        UnityWebRequest www = UnityWebRequestTexture.GetTexture(url);
+        var asyncOperation = www.SendWebRequest();
+
+        while (!asyncOperation.isDone)
+        {
+            await Task.Delay(100);//等待100毫秒，避免卡住主線程
+        }
+
+        if (www.result == UnityWebRequest.Result.Success)
+        {
+            // 載入成功，將下載的紋理轉換為Sprite
+            Texture2D texture = ((DownloadHandlerTexture)www.downloadHandler).texture;
+            sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+        }
+        else
+        {
+            Debug.LogError($"載入url圖片失敗:{www.error}");
+        }
+
+        return sprite;
+    }
+
     /// <summary>
     /// 產生QRCode
     /// </summary>
