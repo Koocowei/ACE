7021770fe0629778ef0840b9082fba260db9c769
jimmy <jimmy@kooco.com.tw>
2024-06-28 01:03:16 +0800

    *新增聊天功能  *Debug使用方式:螢幕繞圈

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -36,6 +36,7 @@ public static class DataManager
     [Header("用戶訊息")]
     public static string UserWalletAddress { get; set; }            //用戶錢包地址
     public static string UserWalletBalance { get; set; }            //用戶錢包餘額
+    public static string UserId { get; set; }                       //用戶ID
     public static string UserNickname { get; set; }                 //用戶暱稱
     public static int UserAvatar { get; set; }                      //用戶頭像
     public static int UserStamina { get; set; }                     //用戶耐力
